f5db55b20d6f88f4c4c3053a899b517437051719
Ranjithubbana <ubbana.ranjith2018@gmail.com>
2024-12-23 12:44:00 -0500

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,30 @@
+create procedure sample
+as
+begin
+select * from [dbo].[DimReseller]
+end
+
+exec sample
+
+select * from DimReseller
+
+select * from DimCustomer
+
+select * from FactInternetSales;
+
+
+create procedure ranjith
+as
+begin
+select D.CustomerKey,
+		CONCAT(D.firstname,'',D.LastName) AS fullname,
+		D.BirthDate,
+		D.EmailAddress,
+		F.UnitPrice,
+		F.TaxAmt
+		from DimCustomer D
+		inner join FactInternetSales F
+		on D.CustomerKey=F.CustomerKey
+		where F.ProductKey='310'
+end;
+
